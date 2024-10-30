32be4892902abd496bbf1ad466d8366d00f834b0
Bach Dao <ngocbach12a1@gmail.com>
2024-10-30 18:48:25 +0700

    video 67: Chức năng View Detail User

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -39,8 +39,9 @@ public class UserController {
 
     @RequestMapping("/admin/user/{id}")
     public String getUserDetailPage(Model model, @PathVariable long id) {
-        System.out.println("Check path id=" + id);
+        User user = this.userService.getUserById(id);
         model.addAttribute("id", id);
+        model.addAttribute("user", user);
         return "admin/user/show";
     }
 
