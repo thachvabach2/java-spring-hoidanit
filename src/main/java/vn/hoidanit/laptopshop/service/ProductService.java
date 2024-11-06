012895667fc228eee4ff07d3c7df32c38df1916f
Bach Dao <ngocbach12a1@gmail.com>
2024-11-06 11:28:14 +0700

    125: Thêm sản phẩm vào giỏ hàng

--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -3,17 +3,30 @@ package vn.hoidanit.laptopshop.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.Cart;
+import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.CartDetailRepository;
+import vn.hoidanit.laptopshop.repository.CartRepository;
 import vn.hoidanit.laptopshop.repository.ProductRepository;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
+    private final CartDetailRepository cartDetailRepository;
+    private final UserService userService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CartRepository cartRepository,
+            CartDetailRepository cartDetailRepository, UserService userService) {
         this.productRepository = productRepository;
+        this.cartRepository = cartRepository;
+        this.cartDetailRepository = cartDetailRepository;
+        this.userService = userService;
     }
 
     public Product saveProduct(Product product) {
@@ -30,5 +43,38 @@ public class ProductService {
 
     public void deleteAProductById(long id) {
         this.productRepository.deleteById(id);
+    }
+
+    public void handleAddProductToCart(String email, long productId) {
+        User user = this.userService.getUserByEmail(email);
+
+        if (user != null) {
+            // cart table
+            Cart cart = this.cartRepository.findByUser(user);
+
+            // Check user có cart table chưa ? do nothing : insert user vào cart table
+            if (cart == null) {
+                // tạo mới cart
+                Cart otherCart = new Cart();
+                otherCart.setUser(user);
+                otherCart.setSum(1);
+                cart = this.cartRepository.save(otherCart);
+            }
+
+            // save card_detail
+            // insert product vào card_detail
+            Optional<Product> product = this.productRepository.findById(productId);
+            if (product.isPresent()) {
+                Product realProduct = product.get();
+
+                CartDetail cartDetail = new CartDetail();
+                cartDetail.setCart(cart);
+                cartDetail.setProduct(realProduct);
+                cartDetail.setPrice(realProduct.getPrice());
+                cartDetail.setQuantity(1);
+
+                this.cartDetailRepository.save(cartDetail);
+            }
+        }
     }
 }
